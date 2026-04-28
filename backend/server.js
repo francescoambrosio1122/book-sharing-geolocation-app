cf5f338a6a067b27a13e52e09a8c34224a34e59b
francescoambrosio1122 <annaambrosiomail@gmail.com>
2026-04-28 16:09:09 +0200

    Aggiunta codice Backed

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log("Backend running");
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// simulazione database libri
+let books = [];
+
+// ottenere tutti i libri
+app.get('/books', (req, res) => {
+  res.json(books);
+});
+
+// aggiungere un libro
+app.post('/books', (req, res) => {
+  const newBook = req.body;
+  books.push(newBook);
+  res.json({ message: "Libro aggiunto", newBook });
+});
+
+app.listen(3000, () => {
+  console.log("Server avviato sulla porta 3000");
+});
